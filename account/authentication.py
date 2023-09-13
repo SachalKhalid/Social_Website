a118532cae2731dbc4fa49680f8aa62e6192c650
SachalKhalid <bcsf20a029@pucit.edu.pk>
2023-09-13 09:45:45 +0500

    Social Atuhentications

---
+++ account/authentication.py
@@ -0,0 +1,6 @@
+from .models import Profile
+def create_profile(backend, user, *args, **kwargs):
+    """
+    Create user profile for social authentication
+    """
+    Profile.objects.get_or_create(user=user)
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-(s#b8_g5x&1fe$257!hg8@-h4$jk2o9srtt3(4!x%hhg)7i%l!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1' ]
 
 
 # Application definition
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,34 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+AUTHENTICATION_BACKENDS = [
+ 'django.contrib.auth.backends.ModelBackend',
+#  'account.authentication.EmailAuthBackend',
+ 'social_core.backends.facebook.FacebookOAuth2',
+]
+
+
+SOCIAL_AUTH_FACEBOOK_KEY = '1471677613564630' # Facebook App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = 'bb743b2f719179b7f5923d973300644c' # Facebook App 
+# SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+
+
+# https://www.facebook.com/v12.0/dialog/oauth?client_id=1471677613564630&redirect_uri=https://mysite.com:8000/social-authcomplete/facebook/&state=c2zEoFVyBJMwgfGdOO01ryvJrmgcOa64&return_scopes=true&scope=email
+
+ 
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
+
+
+     
